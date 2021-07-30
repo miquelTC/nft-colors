1197fa96422aac52084fe0d1b0e1039291e8db10
miquelTC <miqueltc@DESKTOP-G55QVS5.localdomain>
2021-07-30 14:20:06 +0200

    create some tests for the ColorNFT smart contract. Also, add SafeMint function

--- src/contracts/ColorNFT.sol
+++ src/contracts/ColorNFT.sol
@@ -2,8 +2,30 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
-contract ColorNFT is ERC721 {
-  constructor() ERC721("Color", "COLOR") {
+contract ColorNFT is ERC721, ERC721Enumerable {
+  string[] public colors;
+  mapping(string => bool) _colorExists;
+
+  constructor() 
+    ERC721("Color", "COLOR") 
+    {
+  }
+
+  function _beforeTokenTransfer(address from, address to, uint256 tokenId) internal override(ERC721, ERC721Enumerable) {
+    super._beforeTokenTransfer(from, to, tokenId);
+  }
+
+  function supportsInterface(bytes4 interfaceId) public view override(ERC721, ERC721Enumerable) returns (bool) {
+    return super.supportsInterface(interfaceId);
+  }
+
+  function safeMint(string memory _color) public {
+    require(!_colorExists[_color], 'The color should be unique');
+    colors.push(_color);
+    uint _id = colors.length;
+    _safeMint(msg.sender, _id);
+    _colorExists[_color] = true;
   }
 }
